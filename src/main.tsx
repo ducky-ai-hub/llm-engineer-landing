6812682a579ad51dfee82916ba3a4c1ae005ac10
Tuan Vu <tuanvd@gmail.com>
2026-06-27 11:54:15 +0700

    Add react type def packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+// @ts-ignore: allow CSS side-effect import without module declarations
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
